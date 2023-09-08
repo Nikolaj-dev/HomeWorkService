c787459f278b1ac3ef0b80e311ed4516e8a3e133
Nickolaj <nikolajpetrishchev@gmail.com>
2023-09-08 19:14:32 +0600

    set up models and admin panel

--- homeworkservice_app/admin.py
+++ homeworkservice_app/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'subject',
+        'given_by',
+        'theme',
+        'issued_date',
+    )
+    list_filter = (
+        'subject',
+        'issued_date',
+        'given_by',
+    )
+    search_fields = (
+        'theme',
+    )
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+    search_fields = (
+        'title',
+    )
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'phone_number',
+        'email',
+    )
+    search_fields = (
+        'full_name',
+    )
--- homeworkservice_app/models.py
+++ homeworkservice_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    theme = models.CharField(max_length=256)
+    task_body = models.TextField()
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    issued_date = models.DateField(auto_now_add=True)
+    expire_date = models.DateField(auto_now_add=True)
+    given_by = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.theme
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=128)
+    teacher = models.ManyToManyField('Teacher')
+
+    def __str__(self):
+        return self.title
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=128)
+    profile_image = models.ImageField(upload_to='teacher_profile_images/', null=True, blank=True)
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+
